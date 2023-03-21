17a65c22c0583ac09e30fe79df9230a749b4902b
coskun unen <coskunen@gmail.com>
2023-03-22 01:09:54 +0300

    complete

--- src/Task.js
+++ src/Task.js
@@ -1,12 +1,20 @@
 import React from "react";
+import { formatDistanceToNow , differenceInDays } from 'date-fns'
+import { tr } from 'date-fns/locale'
 
 const Task = ({ taskObj, onComplete }) => {
-
-  return (
+const dateToNow = formatDistanceToNow(new Date(taskObj.deadline), {locale: tr});
+const diffDays =  differenceInDays(new Date(taskObj.deadline), new Date())
+const markAsRed = diffDays <= 3 ? true : false ;
+return (
     <div className="task">
       <h3>{taskObj.title}</h3>
-      <div className="deadline">son teslim: <span>{taskObj.deadline}</span></div>
-      <p>{taskObj.description}</p>
+      <div className="deadline">son teslim: -{" "}
+    
+      <span className= {markAsRed ? "bg-orange-400":"bg-[#ffd9d4]" }>{dateToNow} - {diffDays}</span>
+      </div>
+      <p>{taskObj.description}
+      </p>
       <div>
         {taskObj.people.map((p) => (
           <span className="pill" key={p}>{p}</span>
